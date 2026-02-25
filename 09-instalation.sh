46108b99db6dbf264b6669156454c3c374553f4b
maddaganesh9798 <madda.ganesh9798@gmail.com>
2026-02-25 18:15:05 +0530

    mysql

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -8,4 +8,19 @@ then
     echo "Please run this script with root priveleges"
 fi
 
-dnf list installed mysql 
+dnf installed git 
+
+# if [ $? -ne 0 ]
+# then
+#     echo "MySQL is not installed...going to install"
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "MySQL installation is failure..please check"
+#         exit 1
+#     else
+#         echo "MySQL installation is success"
+#     fi
+# else
+#     echo "MySQL is already installed..nothing to do"
+# fi
