751e8dad32d1c5d2967c083e4cc955808d03b80e
maddaganesh9798 <madda.ganesh9798@gmail.com>
2026-02-25 18:05:03 +0530

    mysql

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-userid=$(id -u)
-if [ $userid -ne 0 ]
+
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+if [ $USERID -ne 0 ]
 then
-    echo"please run this script with root privaliges"
+    echo "Please run this script with root priveleges"
 fi
-dnf list installed mysql
 
-# USERID=$(id -u)
-# #echo "User ID is: $USERID"
-
-# if [ $USERID -ne 0 ]
-# then
-#     echo "Please run this script with root priveleges"
-#     exit 1
-# fi
-
-# dnf list installed git # Just checking whether installed or not
+dnf list installed mysql 
