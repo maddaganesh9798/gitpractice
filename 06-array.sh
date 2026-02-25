33d422d9d89c769eaffa9bea254460796092c0b8
maddaganesh9798 <madda.ganesh9798@gmail.com>
2026-02-25 16:49:59 +0530

    array

--- 06-array.sh
+++ 06-array.sh
@@ -5,3 +5,5 @@ firuts=("apple" "orange" "mango")
 echo "First fruit is: ${firuts[0]}"
 echo "Second fruit is: ${firuts[1]}"
 echo "Third fruit is: ${firuts[2]}"
+
+echo "First fruit is: ${firuts[@]}"
